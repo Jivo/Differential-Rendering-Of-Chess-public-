7f8ab730cfb164b009bfcbc6f9d82053288c0752
brandon103 <49328648+brandon103@users.noreply.github.com>
2019-06-06 11:13:21 +1200

    pbrt

---
+++ pbrt-v3/o_src/integrators/mask.cpp
@@ -0,0 +1,89 @@
+#pragma once
+
+/*
+    pbrt source code is Copyright(c) 1998-2016
+                        Matt Pharr, Greg Humphreys, and Wenzel Jakob.
+
+    This file is part of pbrt.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+    - Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    - Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+// integrators/whitted.cpp*
+#include "camera.h"
+#include "film.h"
+#include "integrators/mask.h"
+#include "interaction.h"
+#include "paramset.h"
+
+#include <iostream>
+namespace pbrt {
+
+// WhittedIntegrator Method Definitions
+Spectrum MaskIntegrator::Li(const RayDifferential &ray, const Scene &scene,
+                               Sampler &sampler, MemoryArena &arena,
+                               int depth) const {
+    Spectrum L(0.);
+    // Find closest ray intersection or return background radiance
+    SurfaceInteraction isect;
+
+    if (!scene.Intersect(ray, &isect)) {
+        for (const auto &light : scene.lights) L += light->Le(ray);
+        return L;
+    }
+
+	Float rgb[] = {1, 1, 1};
+
+	if (!scene.aggregate->isLocalPlane) {
+        rgb[1] = 0;
+        rgb[2] = 0;
+	}
+
+    L = Spectrum::FromRGB(rgb);
+
+    return L;
+}
+
+MaskIntegrator *CreateMaskIntegrator(
+    const ParamSet &params, std::shared_ptr<Sampler> sampler,
+    std::shared_ptr<const Camera> camera) {
+    int np;
+    const int *pb = params.FindInt("pixelbounds", &np);
+    Bounds2i pixelBounds = camera->film->GetSampleBounds();
+    if (pb) {
+        if (np != 4)
+            Error("Expected four values for \"pixelbounds\" parameter. Got %d.",
+                  np);
+        else {
+            pixelBounds = Intersect(pixelBounds,
+                                    Bounds2i{{pb[0], pb[2]}, {pb[1], pb[3]}});
+            if (pixelBounds.Area() == 0)
+                Error("Degenerate \"pixelbounds\" specified.");
+        }
+    }
+    return new MaskIntegrator(0, camera, sampler, pixelBounds);
+}
+
+}  // namespace pbrt
